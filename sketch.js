fd1ec7ea0b4ea8ff09c94eaa151d3af4602d317a
Harpreet-hash <73457305+Harpreet-hash@users.noreply.github.com>
2020-11-12 16:33:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 var bullet, wal,speed,weight,damage,thickness;
 function setup() {
   createCanvas(1600,400);
-
+ 
   speed=random(223,331)
   weight=random(30,52)
   thickness=random(22,83)
@@ -23,23 +23,28 @@ console.log(damage)
 
 function draw() {
   background(0,0,0);  
-  
-  
-  
-  if(bullet.isTouching(wall)){
+  if(hasCollided(bullet,wall)){
 
-    bullet.velocityX=0
-    
+    bullet.velocityX=0;
     if(damage>10){
       bullet.shapeColor=rgb(255,0,0)
       
     }else {
       bullet.shapeColor=rgb(0,255,0)     
     }
-    
 
+
+  }  
+  
+  drawSprites();
+}
+function hasCollided(bullet,wall){
+  if(wall.x-bullet.x<(bullet.width+wall.width)/2){
+    return true;    
   }
+  
 
+}
   
   
   drawSprites();
